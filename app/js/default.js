0f96eb218e681e286c5843c4967248ee34c831ba
Cole Smith <coletrainwvu@gmail.com>
2019-01-17 19:46:04 -0500

    finished documenting sliders and created modal, but it still needs some tweaking

--- app/js/default.js
+++ app/js/default.js
@@ -18,16 +18,73 @@ $(document).ready(function(){
     xLarge: 650,
     xxLarge: 800
   }); 
- */
-  
-  $('.slider').slick({
-    dots: true, 
-    infinite: true,
-    speed: 300,    
-    slidesToShow: 3,
-    slidesToScroll: 3 
-    
-  });   
 
+var modal = document.getElementById('modal-1');
+var btn = document.getElementById('modalButton');
+var span = document.getElementsByClassName("close")[0];
+
+//open the modal on btn click
+btn.onClick = function(){ 
+  modal.style.display = "block";
+}
+
+//when the user clicks on <span> (X), close the modal
+span.onclick = function(){
+  modal.style.display = "none";
+}
+
+//when the user clicks anywhere outside the modal, it closes
+window.onclick = function(event){
+  if(event.target == modal){
+    modal.style.display = "none";
+  }
+}
+ 
+  */
+
+ function modal(modalButton, modalContainer){
+     var var_modalButton = $(modalButton),
+         var_modalContainer = $(modalContainer);
+
+  $(var_modalButton).click( function(){
+    alert(var_modalButton);
+    $(var_modalContainer).css("display", "block" );
+  });
+  
+  $(var_modalContainer + ' .close').click( function(){ 
+    $(var_modalContainer).css("display", "none");
+  });
+  
+  $('body').click(function(e){
+    if( $(e.target).closest('#modal-1 .modal__content, #modalButton').length === 0){
+      $("#modal-1").css("display", "none");
+    }
+  });
+}
+
+modal( '#modal-1', '#modalButton');
+
+  
+
+
+
+  
  
 });
+
+
+
+/*
+$('#modalButton').click( function(){
+  $('#modal-1').css("display", "block" );
+});
+
+$('#modal-1 .close').click( function(){ 
+  $("#modal-1").css("display", "none");
+});
+
+$('body').click(function(e){
+  if( $(e.target).closest('#modal-1 .modal__content, #modalButton').length === 0){
+    $("#modal-1").css("display", "none");
+  }
+});*/
